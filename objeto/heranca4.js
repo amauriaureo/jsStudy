395f70e743f8db13403bc5bb9a9f788cf5d7e47f
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-28 20:14:43 -0300

    mais heranca

---
+++ objeto/heranca4.js
@@ -0,0 +1,28 @@
+function MeuObjeto() {}
+console.log(MeuObjeto.prototype)
+
+const obj1 = new MeuObjeto
+const obj2 = new MeuObjeto
+console.log(obj1.__proto__ === obj2.__proto__)
+console.log(MeuObjeto.protype === obj1.__proto__)
+
+MeuObjeto.prototype.nome = 'Ana'
+MeuObjeto.prototype.falar = function() {
+    console.log(`Bom dia! Meu nome é ${this.nome}!`)
+}
+
+obj1.falar()
+obj2.nome = 'Rafa'
+obj2.falar()
+
+const obj3 = {}
+obj3.__proto__ = MeuObjeto.prototype
+obj3.nome = 'Obj3'
+obj3.falar()
+
+// Resumo
+console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
+console.log(MeuObjeto.__proto__ === Function.prototype)
+console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+
