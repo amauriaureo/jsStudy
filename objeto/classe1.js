0c2261b1225a4698e497fae8bd7dc0e41657db4a
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-28 22:42:28 -0300

    classe 1

---
+++ objeto/classe1.js
@@ -0,0 +1,32 @@
+class Lancamento {
+    constructor(nome = 'Genérico', valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach( l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidado += l.valor
+        })
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamento('Salario', 4500)
+const contaDeLuz = new Lancamento('Luz', -220)
+
+const contas = new CicloFinanceiro
+contas.addLancamentos(salario, contaDeLuz)
+console.log(contas.sumario())
