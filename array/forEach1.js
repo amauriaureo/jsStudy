73a0330d9e36ad687394b9e1fa5ef980aa459e6f
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-30 21:40:30 -0300

    integrando forEach em um arquivo

--- array/forEach1.js
+++ array/forEach1.js
@@ -1,9 +1,31 @@
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 
 aprovados.forEach(function(nome, indice) {
-    console.log(`${indice + 1} ${nome}`)
+    console.log(`${indice + 1}. ${nome}`)
 })
 
 // aprovados.forEach(nome => console.log(nome))
-const exibirAprovados = aprovado => console.log(aprovado)
+// const exibirAprovados = aprovado => console.log(aprovado)
 // aprovados.forEach(exibirAprovados)
+
+
+
+// ---------------------------------------------------------------------------------
+
+
+// Array.prototype.forEach2 = function(callback) {
+//     for(let i = 0; i < this.length; i++) {
+//         callback(this[i], i, this)
+//     }
+// }
+
+const checked = ['JS', 'HTML', 'CSS', 'Metodologias ageis', 'Git']
+
+// checked.forEach2(function(nome, indice, array) {
+//     console.log(`${indice + 1}) ${nome}`)
+//     console.log(array)
+// })
+console.log('        Ferramentas:')
+checked.forEach(function(tech, indice) {
+    console.log(`${indice + 1})) ${tech}`)
+})
