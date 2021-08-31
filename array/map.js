8a1e5389a720c1c17c6f2e8232136ae75aec5577
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-30 21:55:30 -0300

    map funcionando

--- array/map.js
+++ array/map.js
@@ -1,2 +1,13 @@
 // passar pelo array original e transformar em outro
-// mapear o array mantendo length mas alterando os dados
+// mapear o array mantendo length mas alterando os dados.
+// transformação de cada elemtno de um array para outro array de mesmo tamanho
+
+
+const nums = [1, 2, 3, 4, 5]
+
+// for com propósito.
+let resultado = nums.map(function(e){
+    return e * 2
+})
+
+console.log(resultado)
