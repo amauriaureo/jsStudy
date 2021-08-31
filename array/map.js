3e1a8e970cdf5202b99f64dd05ab6406cf926190
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-30 22:04:57 -0300

    brincado com map

--- array/map.js
+++ array/map.js
@@ -1,6 +1,7 @@
 // passar pelo array original e transformar em outro
 // mapear o array mantendo length mas alterando os dados.
 // transformação de cada elemtno de um array para outro array de mesmo tamanho
+// não transforma o array original, apenas forma um novo
 
 
 const nums = [1, 2, 3, 4, 5]
@@ -10,4 +11,11 @@ let resultado = nums.map(function(e){
     return e * 2
 })
 
+console.log(resultado)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const monetizando = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
+
+resultado = nums.map(soma10).map(triplo).map(monetizando)
 console.log(resultado)
