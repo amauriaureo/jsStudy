c2ff5de7e6d39fadea39ba29097e1877d7543afe
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-30 22:15:16 -0300

    pequeno exercicio map

--- array/map.js
+++ array/map.js
@@ -3,7 +3,6 @@
 // transformação de cada elemtno de um array para outro array de mesmo tamanho
 // não transforma o array original, apenas forma um novo
 
-
 const nums = [1, 2, 3, 4, 5]
 
 // for com propósito.
@@ -11,11 +10,31 @@ let resultado = nums.map(function(e){
     return e * 2
 })
 
-console.log(resultado)
+// console.log(resultado)
 
 const soma10 = e => e + 10
 const triplo = e => e * 3
 const monetizando = e => `R$ ${parseFloat(e).toFixed(2).replace('.', ',')}`
 
 resultado = nums.map(soma10).map(triplo).map(monetizando)
-console.log(resultado)
+// console.log(resultado)
+
+
+
+
+// ---------------------------------------------------------------------------------
+
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45}',
+    '{"nome": "Caderno", "preco": 13.90}',
+    '{"nome": "Kit de Lapis", "preco": 41.22}',
+    '{"nome": "Caneta", "preco": 7.50}',
+]
+
+// Retornar um array apenas com os preços.
+
+const paraObjeto = json => JSON.parse(json) //converte um texto para objeto
+const apenasPreco = produto => produto.preco
+
+const result = carrinho.map(paraObjeto).map(apenasPreco)
+console.log(result)
