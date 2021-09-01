8564634489306976bf87e5dca7d9b05dd37c5754
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-31 23:34:39 -0300

    funcionamento do filter

---
+++ array/filter2.js
@@ -0,0 +1,21 @@
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        if(callback(this[i], i, this)) {
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
+
+const produtos = [
+    {nome: 'Notebook', preco: 2499, fragil: true},
+    {nome: 'iPad Pro', preco: 4199, fragil: true},
+    {nome: 'Copo de Vidro', preco: 12.49, fragil: true},
+    {nome: 'Corpo de Plástico', preco: 18.99, fragil: false}
+]
+
+const caro = produto => produto.preco > 25
+const fragil = produto => produto.fragil
+console.log(produtos.filter(caro).filter(fragil))
+
