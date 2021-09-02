f99425516ca59e19d8273be6b3072278cfae9a93
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-09-01 23:21:12 -0300

    reduce com valor inicial

--- array/reduce3.js
+++ array/reduce3.js
@@ -1,7 +1,7 @@
-Array.prototype.reduce2 = function(callback) {
-    
-    let acumulador = this[0]
-    for (let i = 1; i < this.length; i++) {
+Array.prototype.reduce2 = function(callback, valorInicial) {
+    const indiceInicial = valorInicial ? 0 : 1
+    let acumulador = valorInicial || this[0]
+    for (let i = indiceInicial; i < this.length; i++) {
         acumulador = callback(acumulador, this[i], i, this)
     }
     return acumulador
@@ -9,4 +9,5 @@ Array.prototype.reduce2 = function(callback) {
 
 const soma = (total, valor) => total + valor
 const nums = [1, 2, 3, 4, 5, 6]
-console.log(nums.reduce2(soma))
+console.log(nums.reduce2(soma, 21))
+
