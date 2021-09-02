c2015befc13d0cc0c3c05d250e9d803d2e85c46d
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-09-01 23:57:32 -0300

    flatMap executado

--- array/flatMap.js
+++ array/flatMap.js
@@ -24,3 +24,11 @@ const getNotasDaTurma = turma => turma.students.map(getNotaAluno)
 const notas1 = school.map(getNotasDaTurma)
 console.log(notas1)
 // um único array com todas as anotas, por isso o flatMap
+
+console.log([['resultado espero é:']].concat([ 8.8, 9.9 ], [ 9.8, 7.9 ]))
+
+Array.prototype.flatMap = function(callback) {
+    return Array.prototype.concat.apply([], this.map(callback))
+}
+const notas2 = school.flatMap(getNotasDaTurma)
+console.log(notas2)
