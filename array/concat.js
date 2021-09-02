4355465efa84aca18b7b1c9668be48debeabde50
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-09-01 23:42:45 -0300

    concatenando

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Xalala', 'Xibala']
+const filhos = ['xururu', 'Xakablau']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhas, filhos)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
