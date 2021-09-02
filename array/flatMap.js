c7ae9a5533779e9c95ef94d7b32a0d0bf75b59bd
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-09-01 23:49:54 -0300

    complementando bloco de arrays

---
+++ array/flatMap.js
@@ -0,0 +1,20 @@
+const school = [{
+    name: 'G1',
+    students: [{
+        name: 'Gus',
+        note: 8.8
+    }, {
+        name: 'Anne',
+        note: 9.9
+    }]
+}, {
+    name: 'G2',
+    students: [{
+        name: 'Geroge',
+        note: 9.8
+    }, {
+        name: 'Marie',
+        note: 7.9
+    }]
+}]
+
