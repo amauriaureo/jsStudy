3c2c23b86de2904dd4be4df76f4d4815ec21bee3
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-09-14 11:51:48 -0300

    ternario

--- fundamentos/ternario.js
+++ fundamentos/ternario.js
@@ -1,3 +1,14 @@
+function result(note) {
+    if(note >= 7){
+        console.log('Aprovado')
+    } else {
+        console.log('Reprovado')
+    }
+}
+result(8)
+
+
+// TERNARIO
 const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado';
 
 console.log(resultado(7.1));
