8d19dc2b52ead8ccc85c7ec09e6cb3bc320db7bb
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-09-14 12:01:07 -0300

    run

--- fundamentos/hoisting.js
+++ fundamentos/hoisting.js
@@ -1,15 +1,15 @@
 // ISSO:
-console.log('a =', a);
+console.log('a =', a); // a = undefined
 var a = 2
-console.log('a =', a);
+console.log('a =', a); // a = 2
 // É IGUAL A ISSO:
 var b
-console.log('b =', b);
+console.log('b =', b); // b = undefined
 b = 2
-console.log('b =', b);
+console.log('b =', b); // b = 2
 // --------------------------------- 
 
 // com let não acontece hoisting
-console.log('c =', c);
+// console.log('c =', c); //ERRO
 let c = 2
-console.log('c =', c);
+console.log('c =', c); // c = 2
