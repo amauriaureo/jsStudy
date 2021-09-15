0db3d4d58ec13fd77ba77f2fedf85a67012f5b96
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-09-15 14:37:54 -0300

    12

---
+++ exercicios/ex12.js
@@ -0,0 +1,10 @@
+function fatorial (numero) {
+    if(numero == 0){
+        return 1
+    } else {
+        return numero * fatorial(numero - 1)
+    }
+}
+
+console.log(fatorial(10))
+console.log(fatorial(0))
